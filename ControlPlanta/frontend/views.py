e4077b01aced7e0123c7c53b4433a2171186c470
Emanuel Zuniga I <emanuzunuga@gmail.com>
2015-06-29 18:39:55 -0600

    0.0.22 front end back up views ajax
    front end back up views ajax

--- ControlPlanta/frontend/views.py
+++ ControlPlanta/frontend/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 import os
 import socket
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
-from dbbackup.uploader import uploadermain
 
 
 class LandingView(TemplateView):
@@ -17,11 +16,14 @@ class TrazabilidadView(TemplateView):
 
 @login_required(login_url='/admin/login/')
 def backupdbmine(request):
-    if socket.gethostname().startswith('Mac'):
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        os.system(BASE_DIR+'/dbbackup/backupdb.sh')
-        return JsonResponse({'status': 'success','system':'MAC','dir':BASE_DIR})
+    if request.is_ajax():
+        if socket.gethostname().startswith('Mac'):
+            BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            os.system(BASE_DIR+'/dbbackup/backupdb.sh')
+            return JsonResponse({'status': 'success','system':'MAC','dir':BASE_DIR})
+        else:
+            #Aqui ejecuto .bat
+            BASE_DIR = os.path.realpath(os.path.dirname(__file__))
+            return JsonResponse({'status': 'Success','system':'Windows','dir':BASE_DIR})
     else:
-        #Aqui ejecuto .bat
-        BASE_DIR = os.path.realpath(os.path.dirname(__file__))
-        return JsonResponse({'status': 'Success','system':'Windows','dir':BASE_DIR})
+        raise Http404  # raise Http404
